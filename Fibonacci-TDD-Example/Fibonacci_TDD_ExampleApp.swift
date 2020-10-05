b27f45e3feb61b85987e744cde2d782ae4dc545c
davidakoontz <david@koontz.name>
2020-10-05 11:52:30 -0500

    Initial Commit

---
+++ Fibonacci-TDD-Example/Fibonacci_TDD_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fibonacci_TDD_ExampleApp.swift
+//  Fibonacci-TDD-Example
+//
+//  Created by David on 10/5/20.
+//
+
+import SwiftUI
+
+@main
+struct Fibonacci_TDD_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fibonacci-TDD-ExampleTests/Fibonacci_TDD_ExampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fibonacci_TDD_ExampleTests.swift
+//  Fibonacci-TDD-ExampleTests
+//
+//  Created by David on 10/5/20.
+//
+
+import XCTest
+@testable import Fibonacci_TDD_Example
+
+class Fibonacci_TDD_ExampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
